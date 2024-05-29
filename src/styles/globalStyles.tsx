f69ed0ccdb87d8175545506f0d7557cbd1e1bf71
staviasz <erickesc21@hotmail.com>
2024-05-28 21:53:24 -0300

    chore: config styled-components

---
+++ src/styles/globalStyles.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+    scrollbar-color: #5c59bb #ebeaed ;
+
+    &::-webkit-scrollbar {
+        width: 10px;
+     }
+
+    &::-webkit-scrollbar-thumb {
+        background: #5c59bb;
+    }
+
+  	&::-webkit-scrollbar-track {
+  		background-color: #ebeaed;
+  	}
+  }
+`;
