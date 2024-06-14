0c25ceaa26d38493face3209fe1c39330b8af56c
Alan Barbosa <alanzinhob@hotmail.com>
2024-06-13 21:18:05 -0300

    FEAT: Create Sorybooks Button and Tests

---
+++ src/components/buttons/ButtonBackPage/test.spec.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+import ButtonBackPage from '.';
+
+// Mock do ícone
+jest.mock('@public/icons/buttonBackPage.svg', () => ({
+  src: 'mocked-icon.svg',
+  height: 24,
+  width: 24,
+}));
+
+describe('Test ButtonBackPage', () => {
+  it('should render ButtonBackPage with link', () => {
+    render(<ButtonBackPage href="/home"/>);
+
+    const button = screen.getByRole('button');
+
+    expect(screen.getByText('Voltar')).toBeInTheDocument();
+    expect(button.closest('a')).toHaveAttribute('href', '/home');
+  });
+
+
+});
