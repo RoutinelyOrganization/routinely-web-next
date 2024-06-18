4960128fced096ddfb59777cb8141695a0219b86
Diogo Caxiado <caxiadodev@gmail.com>
2024-06-18 15:39:36 -0300

    feat: create tests for logo component

---
+++ src/components/logos/Logo/test.spec.tsx
@@ -0,0 +1,35 @@
+import { render, screen } from '@testing-library/react';
+import Logo from '.';
+
+// Mock do ícone
+jest.mock('@public/icons/logo_horizontal.svg', () => ({
+  src: 'mocked-icon.svg',
+  height: 24,
+  width: 24,
+}));
+
+describe('Test Logo', () => {
+  it('should render Logo component with correct alt text', () => {
+    render(<Logo />);
+    const logoElement = screen.getByAltText('logo Routnely');
+    expect(logoElement).toBeInTheDocument();
+  });
+
+  it('should render Logo component with a link', () => {
+    render(<Logo />);
+    const linkElement = screen.getByRole('link');
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it('should render Logo component with correct src', () => {
+    render(<Logo />);
+    const logoElement = screen.getByAltText('logo Routnely');
+    expect(logoElement).toHaveAttribute('src', 'mocked-icon.svg');
+  });
+
+  it('should redirect to the home when logo is clicked', () => {
+    render(<Logo />);
+    const logoElement = screen.getByRole('link');
+    expect(logoElement).toHaveAttribute('href', '/');
+  });
+});
