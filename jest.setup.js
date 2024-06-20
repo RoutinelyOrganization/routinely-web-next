d90dd40c12e60a613e1f888befaf6d4032df9333
staviasz <erickesc21@hotmail.com>
2024-06-20 17:47:04 -0300

    chore: added lib for storybook and react. change mock next/image in jest.setup, block rule in eslint

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,7 @@
+/* eslint-disable @next/next/no-img-element */
 import '@testing-library/jest-dom';
 
-jest.mock('next/image')
+jest.mock('next/image', () => {
+  const MockedImage = () => <img src="mocked-icon.svg" alt="Mocked Icon" />;
+  return MockedImage;
+});
