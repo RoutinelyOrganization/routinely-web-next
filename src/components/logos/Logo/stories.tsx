e92a76a7b744e7208dd40836ba933bd41d206354
Diogo Caxiado <caxiadodev@gmail.com>
2024-06-21 17:27:03 -0300

    refactor: improving the storybook

--- src/components/logos/Logo/stories.tsx
+++ src/components/logos/Logo/stories.tsx
@@ -3,24 +3,12 @@ import Logo from '.';
 export default {
   title: 'Logo',
   component: Logo,
-  args: {
-    children: 'Storybook',
-  },
-  argTypes: {
-    children: { type: 'string' },
-  },
-  parameters: {
-    backgrounds: {
-      values: [
-        {
-          name: 'primary',
-          value: '#000',
-        },
-      ],
-    },
-  },
 };
 
-export const Template = (args: ArgTypes) => {
-  return <Logo {...args} />;
+export const Template = () => {
+  return <Logo />;
+};
+
+Template.parameters = {
+  backgrounds: { default: 'primary' },
 };
