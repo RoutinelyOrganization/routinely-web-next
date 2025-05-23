c9dc287fe0a577edcbaa867dc2ab0e656f6eadcc
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-05-23 16:24:31 -0300

    refactor: adjust tests to have only 2 buttons

--- src/components/containers/ContainerHomePage/ContainerHomePage.spec.tsx
+++ src/components/containers/ContainerHomePage/ContainerHomePage.spec.tsx
@@ -8,11 +8,9 @@ describe('', () => {
     expect(logo).toBeInTheDocument();
 
     const buttons = screen.getAllByRole('button');
-    expect(buttons).toHaveLength(4);
-    expect(buttons[0]).toHaveTextContent('Recursos');
-    expect(buttons[1]).toHaveTextContent('Planos');
-    expect(buttons[2]).toHaveTextContent('Acesse');
-    expect(buttons[3]).toHaveTextContent('Teste de graça');
+    expect(buttons).toHaveLength(2);
+    expect(buttons[0]).toHaveTextContent('Acesse');
+    expect(buttons[1]).toHaveTextContent('Teste de graça');
 
     const headding = screen.getByRole('heading', {
       name: 'Domine sua rotina e conquiste o dia com nossa ferramenta Routinely.',
