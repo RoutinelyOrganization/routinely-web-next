942198e4e7017bb18ea779d28de1a3e24c6bc3a4
GabJen <gabjenramos@gmail.com>
2025-12-08 11:58:10 -0300

    style: remove unnecessary comment

--- mocks/taskMock.ts
+++ mocks/taskMock.ts
@@ -2,16 +2,6 @@ import { Categories } from '@/types/categories';
 import type { Task } from '@/types/task';
 import { stringToDate } from '@/utils/formats/stringToDate';
 
-/**
- * MOCK DATA - Datas estáticas para testes
- *
- * Usando datas estáticas para evitar problemas de fuso horário e garantir
- * que os testes sejam bem-sucedidos em qualquer ambiente.
- *
- * Se esses testes falharem por problemas de data no passado,
- * atualize as strings de data para uma data futura.
- */
-
 const { longDateString: dateNowStr, day, month, year } = stringToDate('2026-06-01 10:00');
 
 const dateTomorrow = stringToDate(`${year}-${month}-${day + 1}`);
